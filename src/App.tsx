c3141ad335af73361b9ca1604c45483854fda5d7
Radim Staněk <gtomyasek@gmail.com>
2024-09-25 18:20:31 +0200

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return <div className="absolute top-0 left-0 h-screen w-screen bg-gray-950">
+      <div className="flex flex-col justify-center items-center gap-4 text-white h-full w-full">
+          <span className="text-3xl">Programátor</span>
+          <h1 className="text-7xl font-bold">Staněk R.</h1>
+          <span className="text-3xl">Mail: <strong>mail@stanekr.cz</strong></span>
+      </div>
+  </div>;
+}
